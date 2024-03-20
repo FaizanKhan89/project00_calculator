9eee891038642bf6e9ba05e64396d3994a942a06
Muhammad Faizan Khan <155994540+FaizanKhan89@users.noreply.github.com>
2024-03-21 02:16:44 +0500

    push my calculator to github

---
+++ index.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node  //npx command
+//SHABANG
+import inquirer from "inquirer";
+//Array[]
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "FirstNumber" },
+    { message: "Enter second number", type: "number", name: "SecondNumber" },
+    {
+        message: "Select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+//Conditional Statement
+if (answer.operator === "Addition") {
+    console.log(answer.FirstNumber + answer.SecondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.FirstNumber - answer.SecondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.FirstNumber * answer.SecondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.FirstNumber / answer.SecondNumber);
+}
+else {
+    console.log("Please Select Valid Operator");
+}
